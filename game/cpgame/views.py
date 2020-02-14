e69030fe0b24880185534ee8e6c6831d54bdd356
adarshraj365@gmail.com <adarshraj365@gmail.com>
2020-02-14 08:26:23 +0530

    bug fixed

--- game/cpgame/views.py
+++ game/cpgame/views.py
@@ -14,12 +14,13 @@ def home(request):
 		time = datetime.datetime.now()
 		time = str(time)
 		time = time[0:19]
-		m = Message()
-		m.name = name
-		m.msg = msg
-		m.time = time
-		m.save()
-		redirect('/')
+		if name and msg and time :
+			m = Message()
+			m.name = name
+			m.msg = msg
+			m.time = time
+			m.save()
+			return redirect('/')
 	return render(request,'home.html')
 
 
